74056d1140b27cf3c8b9662f84ea6c793c814d31
Kevin Lopez <kelodepi@gmail.com>
2022-08-17 20:49:54 -0500

    Agregando Carrito V1

--- js/app.js
+++ js/app.js
@@ -1,27 +1,26 @@
 let op;
 let suma = 0;
-let carrito = "Productos seleccionados: \n";
+const carrito = [];
+let n=0;
 
-function mostrarCarrito(){
-    alert("Este es tu carrito: \n" + carrito + "El total de tus productos es: $ " + suma + " MXN");
-}
 
 class Producto {
-    constructor(nombre, precio, descripcion, categoria, imagen) {
+    constructor(nombre, precio, descripcion, categoria, imagen, id) {
         this.nombre = nombre;
         this.precio = precio;
         this.descripcion = descripcion;
         this.categoria = categoria;
-        this.imagen = imagen;}
+        this.imagen = imagen;
+        this.id = id;}
 };
 
-const producto1 = new Producto("Monitor LG", 3800, 'Monitor 24"', "Monitor", "./img/android-chrome-192x192.png");
-const producto2 = new Producto("Mouse Asus", 620, 'Mouse óptico 3500 DPI', "Mouse", "./img/android-chrome-192x192.png");
-const producto3 = new Producto("Teclado Balam Rush", 1800, "Teclado mecánico retroiluminado", "Teclado", "./img/android-chrome-192x192.png");
-const producto4 = new Producto("Impresora HP", 4200, "Impresora blanco y negro", "Impresora", "./img/android-chrome-192x192.png");
-const producto5 = new Producto("Cámara Canon", 7159, "Cámara Canon E5", "Cámara", "./img/android-chrome-192x192.png");
-const producto6 = new Producto("Monitor Lenovo", 4500, 'Monitor 26"', "Monitor", "./img/android-chrome-192x192.png");
-const producto7 = new Producto("Monitor Asus", 6149, 'Monitor 28"', "Monitor", "./img/android-chrome-192x192.png");
+const producto1 = new Producto("Monitor LG", 3800, 'Monitor 24"', "Monitor", "./img/android-chrome-192x192.png", 1);
+const producto2 = new Producto("Mouse Asus", 620, 'Mouse óptico 3500 DPI', "Mouse", "./img/android-chrome-192x192.png", 2);
+const producto3 = new Producto("Teclado Balam Rush", 1800, "Teclado mecánico retroiluminado", "Teclado", "./img/android-chrome-192x192.png", 3);
+const producto4 = new Producto("Impresora HP", 4200, "Impresora blanco y negro", "Impresora", "./img/android-chrome-192x192.png", 4);
+const producto5 = new Producto("Cámara Canon", 7159, "Cámara Canon E5", "Cámara", "./img/android-chrome-192x192.png", 5);
+const producto6 = new Producto("Monitor Lenovo", 4500, 'Monitor 26"', "Monitor", "./img/android-chrome-192x192.png", 6);
+const producto7 = new Producto("Monitor Asus", 6149, 'Monitor 28"', "Monitor", "./img/android-chrome-192x192.png", 7);
 
 const productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7];
 
@@ -34,7 +33,7 @@ productos.forEach((producto) => {
         <img src="./img/android-chrome-192x192.png" class="imagenProducto">
         <p class="descripcionProducto">${producto.descripcion}</p>
         <span class="precioProducto"> $ ${producto.precio} MXN </span>
-        <button class="botonAgregarCarrito"> Agregar al Carrito </button>
+        <button class="botonAgregarCarrito" data-id="${producto.id}"> Agregar al Carrito </button>
         `
     cardProducto.className = 'cardContainer'
     cardContainerQuery.append(cardProducto)
@@ -42,8 +41,8 @@ productos.forEach((producto) => {
 
 const botonFiltrar = document.querySelector('#botonFiltrar')
 const filtrar = () => {
-    const eliminarPorClase = document.querySelectorAll('.cardContainer');
-    eliminarPorClase.forEach((elemento) => {
+    const eliminarProductos = document.querySelectorAll('.cardContainer');
+    eliminarProductos.forEach((elemento) => {
         elemento.remove();
     });
     const cbMonitor = document.querySelector('#monitorFilter');
@@ -61,7 +60,7 @@ const filtrar = () => {
                 <img src="./img/android-chrome-192x192.png" class="imagenProducto">
                 <p class="descripcionProducto">${producto.descripcion}</p>
                 <span class="precioProducto"> $ ${producto.precio} MXN </span>
-                <button class="botonAgregarCarrito"> Agregar al Carrito </button>
+                <button class="botonAgregarCarrito" data-id="${producto.id}"> Agregar al Carrito </button>
                 `
             cardProducto.className = 'cardContainer'
             cardContainerQuery.append(cardProducto)
@@ -76,7 +75,7 @@ const filtrar = () => {
                 <img src="./img/android-chrome-192x192.png" class="imagenProducto">
                 <p class="descripcionProducto">${producto.descripcion}</p>
                 <span class="precioProducto"> $ ${producto.precio} MXN </span>
-                <button class="botonAgregarCarrito"> Agregar al Carrito </button>
+                <button class="botonAgregarCarrito" data-id="${producto.id}"> Agregar al Carrito </button>
                 `
             cardProducto.className = 'cardContainer'
             cardContainerQuery.append(cardProducto)
@@ -91,7 +90,7 @@ const filtrar = () => {
                 <img src="./img/android-chrome-192x192.png" class="imagenProducto">
                 <p class="descripcionProducto">${producto.descripcion}</p>
                 <span class="precioProducto"> $ ${producto.precio} MXN </span>
-                <button class="botonAgregarCarrito"> Agregar al Carrito </button>
+                <button class="botonAgregarCarrito" data-id="${producto.id}"> Agregar al Carrito </button>
                 `
             cardProducto.className = 'cardContainer'
             cardContainerQuery.append(cardProducto)
@@ -106,7 +105,7 @@ const filtrar = () => {
                 <img src="./img/android-chrome-192x192.png" class="imagenProducto">
                 <p class="descripcionProducto">${producto.descripcion}</p>
                 <span class="precioProducto"> $ ${producto.precio} MXN </span>
-                <button class="botonAgregarCarrito"> Agregar al Carrito </button>
+                <button class="botonAgregarCarrito" data-id="${producto.id}"> Agregar al Carrito </button>
                 `
             cardProducto.className = 'cardContainer'
             cardContainerQuery.append(cardProducto)
@@ -121,7 +120,7 @@ const filtrar = () => {
                 <img src="./img/android-chrome-192x192.png" class="imagenProducto">
                 <p class="descripcionProducto">${producto.descripcion}</p>
                 <span class="precioProducto"> $ ${producto.precio} MXN </span>
-                <button class="botonAgregarCarrito"> Agregar al Carrito </button>
+                <button class="botonAgregarCarrito" data-id="${producto.id}"> Agregar al Carrito </button>
                 `
             cardProducto.className = 'cardContainer'
             cardContainerQuery.append(cardProducto)
@@ -135,7 +134,7 @@ const filtrar = () => {
                 <img src="./img/android-chrome-192x192.png" class="imagenProducto">
                 <p class="descripcionProducto">${producto.descripcion}</p>
                 <span class="precioProducto"> $ ${producto.precio} MXN </span>
-                <button class="botonAgregarCarrito"> Agregar al Carrito </button>
+                <button class="botonAgregarCarrito" data-id="${producto.id}"> Agregar al Carrito </button>
                 `
             cardProducto.className = 'cardContainer'
             cardContainerQuery.append(cardProducto)
@@ -156,8 +155,8 @@ const borrarFiltro = () => {
     cbTeclado.checked = false;
     cbImpresora.checked = false;
     cbCamara.checked = false;
-    const eliminarPorClase = document.querySelectorAll('.cardContainer');
-    eliminarPorClase.forEach((elemento) => {
+    const eliminarProductos = document.querySelectorAll('.cardContainer');
+    eliminarProductos.forEach((elemento) => {
         elemento.remove();
     });
     productos.forEach((producto) => {
@@ -167,10 +166,38 @@ const borrarFiltro = () => {
             <img src="./img/android-chrome-192x192.png" class="imagenProducto">
             <p class="descripcionProducto">${producto.descripcion}</p>
             <span class="precioProducto"> $ ${producto.precio} MXN </span>
-            <button class="botonAgregarCarrito"> Agregar al Carrito </button>
+            <button class="botonAgregarCarrito" data-id="${producto.id}> Agregar al Carrito </button>
             `
         cardProducto.className = 'cardContainer'
         cardContainerQuery.append(cardProducto)
     })
 }
 botonBorrarFiltro.addEventListener('click', borrarFiltro)
+
+
+
+const botonesAgregarCarrito = document.querySelectorAll('.botonAgregarCarrito')
+
+const agregarAlCarro = (datosDelEvento) => {
+    const idProductoAgregado = datosDelEvento.target.getAttribute('data-id');
+    const productoAgrgado = productos.find((produc) => produc.id == idProductoAgregado);
+    carrito.push(productoAgrgado);
+    console.log(carrito);
+}
+botonesAgregarCarrito.forEach((boton) => {
+    boton.addEventListener('click', agregarAlCarro);
+})
+
+const VerCarro = () => {
+    carrito.forEach((producto) =>{
+        n = Number(sessionStorage.getItem('n'))
+        console.log(producto);
+        enJSON = JSON.stringify(producto);
+        sessionStorage.setItem(`producto`+ n, enJSON);
+        n++;
+        sessionStorage.setItem('n', n)
+    }) 
+}
+const botonVerCarrito = document.querySelector('#botonVerCarrito')
+botonVerCarrito.addEventListener('click', VerCarro );
+
